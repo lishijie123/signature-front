38ee990d4a92af538606b1f48e749b8a0a945c85
张新飞 <zhangxfyf@seeyon.com>
2022-11-30 15:49:56 +0800

    添加ant，布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import Test from './components/Test.vue'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <Test></Test>
-  <RouterView></RouterView>
+  <a-config-provider>
+    <router-view></router-view>
+  </a-config-provider>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     }
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [ AntDesignVueResolver() ],
+    })
+  ],
   server: {
     open: true,
     proxy: {
