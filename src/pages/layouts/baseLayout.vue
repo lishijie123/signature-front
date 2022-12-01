56aaa0d8e861da44d008d185ed94bfb76cb05717
李士杰 <lisj@seeyon.com>
2022-12-01 10:52:19 +0800

    修改baselayout

--- src/pages/layouts/baseLayout.vue
+++ src/pages/layouts/baseLayout.vue
@@ -1,9 +1,5 @@
 <script setup>
-  import { PieChartOutlined, DesktopOutlined, UserOutlined, TeamOutlined, FileOutlined } from '@ant-design/icons-vue';
   import Menu from '@/components/menu/Menu.vue'
-
-  
-  
 </script>
 
 <template>
@@ -14,14 +10,6 @@
       <a-layout-content style="margin: 0 16px">
         <router-view></router-view>
       </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
     </a-layout>
   </a-layout>
 </template>
-
-
-  <style>
-
-  </style>
